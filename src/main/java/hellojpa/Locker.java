723c12d5af3e9f87a4682ca5ca28761f943f73a0
DevJDra <jyahn3752@gmail.com>
2022-07-27 23:26:04 +0900

    2022-07-26 상속관계매핑 commit

--- src/main/java/hellojpa/Locker.java
+++ src/main/java/hellojpa/Locker.java
@@ -1,9 +1,6 @@
 package hellojpa;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.OneToOne;
+import javax.persistence.*;
 
 @Entity
 public class Locker {
@@ -15,4 +12,28 @@ public class Locker {
 
     @OneToOne(mappedBy = "locker")
     private Member member;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Member getMember() {
+        return member;
+    }
+
+    public void setMember(Member member) {
+        this.member = member;
+    }
 }
